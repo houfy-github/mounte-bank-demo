0fa5204a1e8f72580b10cfd44fac96d3ab4860f8
yycao <yycao@thoughtworks.com>
2017-07-18 14:01:20 +0800

    [NA] remove unused part

--- docker-demo/start.sh
+++ docker-demo/start.sh
@@ -2,21 +2,18 @@
 
 set -e
 RUN_RESULT=$(docker ps | grep hasanozgan/mountebank | wc -l)
+MOUNTEBANK_URI=http://localhost:2525
+BANK_IS_OPEN=1
+
 if [ "$RUN_RESULT" -eq 0 ]; then
   docker run -p 2525:2525 -p 4545:4545 -d hasanozgan/mountebank
 fi
-MOUNTEBANK_URI=http://localhost:2525
 
-for i in 1 2 3 4 5 6 7 8 9 10
-do
-  BANK_IS_OPEN=1
-  curl $MOUNTEBANK_URI/imposters || BANK_IS_OPEN=0
-  if [ $BANK_IS_OPEN -eq 1 ]; then
-    break
-  fi
-  echo "Mountebank is not running. Trying again..."
-  sleep 1
-done
+curl $MOUNTEBANK_URI/imposters || BANK_IS_OPEN=0
+if [ $BANK_IS_OPEN -eq 1 ]; then
+  break
+fi
+
 curl -X DELETE $MOUNTEBANK_URI/imposters/4545
 curl -X POST -H 'Content-Type: application/json' -d @stubs.json $MOUNTEBANK_URI/imposters
 
